910d4b9dfd2475630000f7a6222182dead9244aa
heshanu <heshanu97@gmail.com>
2022-08-01 02:00:06 +0530

    Addded my work

---
+++ react-springboot-microservices-master/user_management_microservice/src/test/java/com/sha/user_management_microservice/UserManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.user_management_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
